716715e482f830e7fa5d675e83eeae2ab21a162a
danielmarko <daniel26tipp@gmail.com>
2024-05-24 13:28:27 +0100

    test

--- src/main/java/ericsson/project/mnrao/MnraoApplication.java
+++ src/main/java/ericsson/project/mnrao/MnraoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MnraoApplication {
-
+	//test CI
 	public static void main(String[] args) {
 		SpringApplication.run(MnraoApplication.class, args);
 	}
