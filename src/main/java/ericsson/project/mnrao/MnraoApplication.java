599f77fc3d5b089fb3b2e7ab3459671df2b3746b
danielmarko <daniel26tipp@gmail.com>
2024-05-24 13:56:01 +0100

    new sonar test

--- src/main/java/ericsson/project/mnrao/MnraoApplication.java
+++ src/main/java/ericsson/project/mnrao/MnraoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MnraoApplication {
-	//test CI test Sonar
+	//test CI new sonar test
 	public static void main(String[] args) {
 		SpringApplication.run(MnraoApplication.class, args);
 	}
